d953a62112a7512a7c5204018f77a3f5f34a7ee4
Alexey Berezutsky <alexeyberezutsky@gmail.com>
2014-01-06 02:55:32 +0300

    Added contracts to deploy

---
+++ Platform/Deploy/Contracts/IDeploying.cs
@@ -0,0 +1,35 @@
+﻿using Deploy.MessageContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace Deploy.Contracts
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public interface IPlayerDeploy
+    {
+        // client module calls deploy and create request to it
+        //one file-one robot- one session
+        // During player deploy this service calls configuration service
+        // it register robot in configuration and uploads robot program to robot directly with 
+        //help of lejos scripts
+  
+        [OperationContract]
+        void DeployPlayer(PlayerDeployInfo request);
+
+        [OperationContract]
+        void CancelDeploy(CancelDeployRequest request);
+
+        [OperationContract]
+        void UnDeploy(CancelDeployRequest request);
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public interface ITeamUndeploy
+    {
+        [OperationContract]
+        void UndeployTeam(UnDeployTeamRequest request);
+    }
+}
---
+++ Platform/Deploy/Contracts/PlayerDeployInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+
+namespace Deploy.MessageContracts
+{
+    [DataContract]
+    public class PlayerDeployInfo : IDisposable
+    {
+        [DataMember]
+        public string FileName;
+
+        [DataMember]
+        public long Length;
+
+        [DataMember]
+        public byte TeamId;
+
+        [DataMember]
+        public byte PlayerId;
+
+        [DataMember]
+        public System.IO.Stream FileByteStream;
+
+        public void Dispose()
+        {
+            if (FileByteStream == null) return;
+            FileByteStream.Close();
+            FileByteStream = null;
+        }
+    }
+
+    [DataContract]
+    public class CancelDeployRequest
+    {
+        [DataMember]
+        public string FileName;
+    }
+
+    [DataContract]
+    public class UnDeployRequest
+    {
+        [DataMember]
+        public byte PlayerId;
+    }
+
+    [DataContract]
+    public class UnDeployTeamRequest
+    {
+        [DataMember]
+        public byte TeamId;
+    }
+}
