a0ebb381ea5d064c1b3576d9f63984c07097261f
Alexey Berezutsky <alexeyberezutsky@gmail.com>
2014-01-08 18:32:16 +0400

    Added sceleton of service. Not decided what to add to analytics

--- Platform/Analytics/AnalyticsService.svc.cs
+++ Platform/Analytics/AnalyticsService.svc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Analytics.ServiceContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -8,26 +9,11 @@ using System.Text;
 
 namespace Analytics
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AnalyticsService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select AnalyticsService.svc or AnalyticsService.svc.cs at the Solution Explorer and start debugging.
-    public class AnalyticsService : IAnalytics
+    public class AnalyticsService : IAnalyticsManager
     {
-        public string GetData(int value)
+        public DataContracts.AnalyticsData GetAnalyticsData()
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Platform/Analytics/Contracts/DeployManager.cs
@@ -0,0 +1,16 @@
+﻿using Analytics.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace Analytics.ServiceContracts
+{
+    [ServiceContract]
+    public interface IAnalyticsManager
+    {
+        [OperationContract]
+        AnalyticsData GetAnalyticsData();
+    }
+}
