a5557bc378a8d4c3e01c84775a7f1d3a5d28bf49
Alexey Berezutsky <alexeyberezutsky@gmail.com>
2014-01-08 18:25:35 +0400

    Suppressed: The service cannot be activated because it does not support ASP.NET compatibility. support of asp net is switched off

--- Platform/Arbiter/ArbiterService.svc.cs
+++ Platform/Arbiter/ArbiterService.svc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Arbiter.ServiceContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -10,24 +11,12 @@ namespace Arbiter
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ArbiterService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select ArbiterService.svc or ArbiterService.svc.cs at the Solution Explorer and start debugging.
-    public class ArbiterService : IArbiter
+    public class ArbiterService : IGameManager
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public void TeamReady(byte TemaId)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            throw new NotImplementedException();
         }
     }
 }
