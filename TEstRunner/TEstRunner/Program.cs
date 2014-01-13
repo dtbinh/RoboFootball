8fb7eae779b4d67e9e68d514bf270f4a3eb28d4f
Alexey Berezutsky <alexeyberezutsky@gmail.com>
2014-01-13 23:14:06 +0400

    VS express could not attach to nunit. I created my own test runner

--- TEstRunner/TEstRunner/Program.cs
+++ TEstRunner/TEstRunner/Program.cs
@@ -1,15 +1,44 @@
-﻿using System;
+﻿using Arbiter.Test;
+using NUnit.Core;
+using NUnit.Core.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace TEstRunner
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            CoreExtensions.Host.InitializeService();
+            TestSuiteBuilder builder = new TestSuiteBuilder();
+            TestPackage testPackage = new TestPackage(@"d:\Programming\RoboFootball\Platform\Arbiter.Test\bin\Debug\Arbiter.Test.dll");
+            RemoteTestRunner remoteTestRunner = new RemoteTestRunner();
+            remoteTestRunner.Load(testPackage);
+
+            remoteTestRunner.Run(new NullListener(), new SingleTestFilter("StatusCallTest"), false, LoggingThreshold.Error);
+        }
+    }
+
+    public class SingleTestFilter : TestFilter
+    {
+        private string testName;
+
+        public SingleTestFilter(string TestName)
+        {
+            testName = TestName;
+        }
+
+
+        public override bool Match(ITest test)
+        {
+            return test.TestName.Name.Equals(testName);
         }
     }
 }
