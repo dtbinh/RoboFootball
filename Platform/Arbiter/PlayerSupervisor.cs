d9c8990c3651ccac7f3c54951fa1ec9c422ceb43
Alexey Berezutsky <alexeyberezutsky@gmail.com>
2014-01-26 23:12:11 +0400

    Finished scetch of supervisorservice

--- Platform/Arbiter/PlayerSupervisor.cs
+++ Platform/Arbiter/PlayerSupervisor.cs
@@ -7,126 +7,6 @@ using System.Web;
 
 namespace Arbiter
 {
-    //public class PlayerSupervisorService
-    //{
-    //    private PlayerSupervisorService();
-
-    //    PlayerSupervisorService instance = null;
-
-    //    private IList<PlayerSupervisor> supervisorsPool;
-
-    //    public IEnumerable<PlayerSupervisor> Supervisors
-    //    {
-    //        get
-    //        {
-    //            return supervisorsPool
-    //                   .Select(s => s.Clone())
-    //                   .Cast<PlayerSupervisor>();
-    //        }
-    //        private set { Supervisors = new List<PlayerSupervisor>(); }
-    //    }
-
-    //    public PlayerSupervisorService Instance
-    //    {
-    //        get
-    //        {
-    //            if (instance == null)
-    //                instance = new PlayerSupervisorService();
-    //            return instance;
-    //        }
-    //    }
-
-    //    public class PlayerSupervisor : ICloneable
-    //    {
-    //        ConfigurationSvc.PlayerData playerData;
-    //        ObserverSvc.PhysicInfo currentPhysicInfo;
-    //        public bool IsSuspended { get; set; }
-    //        public bool IsRun { get; set; }
-    //        Thread thread;
-
-    //        protected PlayerSupervisor(ConfigurationSvc.PlayerData player)
-    //        {
-    //            if (player == null) throw new ArgumentException("Player data should not be null");
-    //            this.playerData = player;
-    //            IsRun = true;
-    //            IsSuspended = false;
-    //            thread = new Thread(this.CheckRules);
-    //            thread.Start();
-    //        }
-
-    //        private PlayerSupervisor()
-    //        { 
-    //        }
-
-    //        public void CheckRules()
-    //        {
-    //            while (IsRun)
-    //            {
-    //                if (IsSuspended) continue;
-    //                //checking logic
-    //            }
-    //        }
-
-    //        public override bool Equals(object obj)
-    //        {
-    //            if (obj.GetType() != typeof(PlayerSupervisor)) return false;
-    //            var ouetrSupervisor = (PlayerSupervisor)obj;
-    //            return this.playerData.Id.Equals(ouetrSupervisor.playerData.Id);
-    //        }
-
-    //        public override int GetHashCode()
-    //        {
-    //            return playerData.Id.GetHashCode();
-    //        }
-
-
-    //        public object Clone()
-    //        {
-    //            return new PlayerSupervisor
-    //            {
-    //                playerData = this.playerData,
-    //                currentPhysicInfo = this.currentPhysicInfo,
-    //                IsSuspended = this.IsSuspended,
-    //                IsRun = this.IsRun
-    //            };
-    //        }
-    //    }
-
-    //    public void SupervisorFactory(ConfigurationSvc.GameMembership mem)
-    //    {
-    //        foreach (var team in mem.Teams)
-    //        {
-    //            foreach (var player in team.Players)
-    //            {
-    //                var supervisor = new PlayerSupervisor(player);
-    //                if (supervisorsPool.Contains(supervisor))
-    //                    supervisorsPool.Add(supervisor);
-    //            }
-    //        }
-    //    }
-
-    //    public static bool ActivateSupervisors(System.Collections.Generic.IEnumerable<PlayerSupervisor> supervisorsToActivate)
-    //    {
-    //        throw new System.NotImplementedException();
-    //    }
-
-    //    public static bool DisactivateSupervisors(System.Collections.Generic.IEnumerable<PlayerSupervisor> supervisorsToDisactivate)
-    //    {
-    //        throw new System.NotImplementedException();
-    //    }
-
-    //    public static bool SuspendSupervisors(System.Collections.Generic.IEnumerable<PlayerSupervisor> supervisorsToActivate)
-    //    {
-    //        throw new System.NotImplementedException();
-    //    }
-
-    //    public static bool StartSupervisors(System.Collections.Generic.IEnumerable<PlayerSupervisor> supervisorsToActivate)
-    //    {
-    //        throw new System.NotImplementedException();
-    //    }
-    //}
-
-
     public class SupervisorsService
     {
         private static SupervisorsService instance;
@@ -141,15 +21,15 @@ namespace Arbiter
 
 
         private static Func<ConfigurationSvc.PlayerData, Supervisor> SpervisorFactory;
-        private ObjectPool<ConfigurationSvc.PlayerData, Supervisor> supervisorsPool;
+        private ObjectContainer<ConfigurationSvc.PlayerData, Supervisor> supervisors;
 
         public class Supervisor
         {
-             ConfigurationSvc.PlayerData playerData;
-             ObserverSvc.PhysicInfo currentPhysicInfo;
-             public bool IsSuspended { get; set; }
-             public bool IsRun { get; set; }
-             Thread thread;
+            ConfigurationSvc.PlayerData playerData;
+            ObserverSvc.PhysicInfo currentPhysicInfo;
+            public bool IsSuspended { get; set; }
+            public bool IsRun { get; set; }
+            Thread thread;
 
             internal static void Initialize()
             {
@@ -160,43 +40,43 @@ namespace Arbiter
                 if (player == null) throw new ArgumentException("Player data should not be null");
                 return new Supervisor(player);
             }
-            
+
             private Supervisor(ConfigurationSvc.PlayerData player)
-             {
-                 this.playerData = player;
-                 IsRun = true;
-                 IsSuspended = false;
-                 thread = new Thread(this.CheckRules);
-                 thread.Start();
-             }
+            {
+                this.playerData = player;
+                IsRun = true;
+                IsSuspended = true;
+                thread = new Thread(this.CheckRules);
+                thread.Start();
+            }
 
-             public void CheckRules()
-             {
-                 while (IsRun)
-                 {
-                     if (IsSuspended) continue;
-                     //checking logic
-                 }
-             }
+            public void CheckRules()
+            {
+                while (IsRun)
+                {
+                    if (IsSuspended) continue;
+                    //checking logic
+                }
+            }
 
-             public override bool Equals(object obj)
-             {
-                 if (obj.GetType() != typeof(Supervisor)) return false;
-                 var ouetrSupervisor = (Supervisor)obj;
-                 return this.playerData.Id.Equals(ouetrSupervisor.playerData.Id);
-             }
+            public override bool Equals(object obj)
+            {
+                if (obj.GetType() != typeof(Supervisor)) return false;
+                var ouetrSupervisor = (Supervisor)obj;
+                return this.playerData.Id.Equals(ouetrSupervisor.playerData.Id);
+            }
 
-             public override int GetHashCode()
-             {
-                 return playerData.Id.GetHashCode();
-             }
+            public override int GetHashCode()
+            {
+                return playerData.Id.GetHashCode();
+            }
         }
 
         private SupervisorsService()
         {
             Supervisor.Initialize();
-            this.supervisorsPool =
-            new ObjectPool<ConfigurationSvc.PlayerData, Supervisor>(CreateSupervisor);
+            this.supervisors =
+            new ObjectContainer<ConfigurationSvc.PlayerData, Supervisor>(CreateSupervisor);
         }
 
         private Supervisor CreateSupervisor(ConfigurationSvc.PlayerData player)
@@ -206,35 +86,65 @@ namespace Arbiter
 
         public Supervisor GetSupervisorFor(ConfigurationSvc.PlayerData player)
         {
-            return supervisorsPool.GetObjectFor<ConfigurationSvc.PlayerData>(player);
+            return supervisors.GetObjectFor<ConfigurationSvc.PlayerData>(player);
+        }
+
+        public IList<Supervisor> CreateSupervisorsFor(ConfigurationSvc.GameMembership mem)
+        {
+            return mem.Teams.SelectMany(t => t.Players.Select(p=>GetSupervisorFor(p))).ToList();
+        }
+
+        public void KillSupervisors(IEnumerable<Supervisor> SupervisorToKill)
+        {
+            foreach (var supervisor in SupervisorToKill)
+            {
+                supervisor.IsRun = false;
+                supervisors.RemoveObject(supervisor);
+            }
+        }
+
+        public void SuspendSupervisors(IEnumerable<Supervisor> SupervisorToSuspend)
+        {
+            foreach (var supervisor in SupervisorToSuspend)
+            {
+                supervisor.IsSuspended = true;
+            }
+        }
+
+        public void StartSupervisors(IEnumerable<Supervisor> supervisorsToStart)
+        {
+            foreach (var supervisor in supervisorsToStart)
+            {
+                supervisor.IsSuspended = false;
+            }
         }
     }
 
 
-    public class ObjectPool<I,T>
+    class ObjectContainer<I,T>
     {
-        private ConcurrentBag<T> objects;
-        private Func<I,T> objectGenerator;
+        private ISet<T> objects;
+        private Func<I, T> objectGenerator;
 
-        public ObjectPool(Func<I,T> objectGenerator)
+        public ObjectContainer(Func<I, T> objectGenerator)
         {
             if (objectGenerator == null) throw new ArgumentNullException("objectGenerator");
-            objects = new ConcurrentBag<T>();
+            objects = new HashSet<T>();
             this.objectGenerator = objectGenerator;
         }
 
         public T GetObjectFor<I>(I param)
         {
-            T item;
-            if (objects.TryTake(out item)) return item;
-            
-            var generatedObject=objectGenerator(param);
-            return generatedObject;
+            var generated = objectGenerator(param);
+            if (!objects.Contains(generated)) objects.Add(generated);
+            return generated;
+            throw new System.NotImplementedException("should return object "); 
         }
 
-        public void PutObject(T item)
+        public bool RemoveObject(T item)
         {
-            objects.Add(item);
+            return objects.Remove(item);
         }
+
     }
 }
